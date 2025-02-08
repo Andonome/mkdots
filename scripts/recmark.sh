cf9b4819ea8fa8f1730fabb739b14c40c0598468
Malin Freeborn <malinfreeborn@posteo.net>
2025-02-08 01:49:52 +0100

    recmark script

---
+++ scripts/recmark.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+art2db () 
+{ 
+    title="$(lowdown -X title "$1" | jq -r)";
+    date="$(lowdown -X date "$1" | cut -dT -f1)";
+    wc="$(wc -w "$1" | cut -d' ' -f1)";
+    tags="$(lowdown -X tags "$1" | jq -r '.[]')";
+
+    recins -f Title -v "$title" \
+        -f Date -v "$date" \
+        -f Tags -v "$tags" \
+        -f WC -v "$wc" \
+        -f File -v "$1" articles.rec
+}
+
+[ -f articles.rec ] || (
+    touch articles.rec
+    echo articles.rec >> .git/info/exclude
+)
+
+for file in *.md; do
+    count="$(recsel -e "File = '${file}'" -c articles.rec)"
+    test "$count" -eq "1" || art2db "$file"
+done
+
