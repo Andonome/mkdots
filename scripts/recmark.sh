2c815cfed7fe2a9308b842fdb9cd3bad27670676
Malin Freeborn <malinfreeborn@posteo.net>
2025-02-08 03:20:49 +0100

    add content to recmark.sh

--- scripts/recmark.sh
+++ scripts/recmark.sh
@@ -4,16 +4,19 @@ set -e
 
 art2db () 
 { 
-    title="$(lowdown -X title "$1" | jq -r)";
+    title="$(lowdown -X title "$1" | jq -r || echo None in $1)";
     date="$(lowdown -X date "$1" | cut -dT -f1)";
     wc="$(wc -w "$1" | cut -d' ' -f1)";
     tags="$(lowdown -X tags "$1" | jq -r '.[]')";
+    content="$(sed '1,5d' "$1")"
 
     recins -f Title -v "$title" \
+        -f File -v "$1" \
         -f Date -v "$date" \
         -f Tags -v "$tags" \
         -f WC -v "$wc" \
-        -f File -v "$1" articles.rec
+        -f Content -v "${content}" \
+        articles.rec
 }
 
 [ -f articles.rec ] || (
