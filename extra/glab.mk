ca4e17d85aedd796c62fbfb9a1d950d8c93caa4e
Malin Freeborn <malinfreeborn@posteo.net>
2025-02-14 17:33:31 +0100

    refactor glab-cli

--- extra/glab.mk
+++ extra/glab.mk
@@ -1,6 +1,18 @@
-default += $(HOME)/.config/glab-cli/config.yml $(HOME)/.config/glab-cli/aliases.yml
+token_name = gitlab-token
 
-$(HOME)/.config/glab-cli/%.yml: extra/glab-cli/%.yml
-	mkdir -p $(@D)
-	chmod 600 $^
-	$(CP) $< $@
+$(HOME)/.config/glab-cli/config.yml: $(HOME)/.password-store/$(token_name).gpg
+	pass $(token_name) | glab auth login --stdin
+	glab config set editor ${VISUAL}
+	glab config set check_update false
+	glab config set browser qutebrowser
+	glab config set display_hyperlinks false
+
+$(HOME)/.config/glab-cli/aliases.yml:
+	glab alias set c 'issue list'
+	glab alias set s 'issue view $1 --comments'
+	glab alias set r 'issue note $1 --message'
+
+secrets += $(HOME)/.config/glab-cli/config.yml
+secrets += $(HOME)/.config/glab-cli/aliases.yml
+
+
