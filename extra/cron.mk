10473871b1aa0c1b7e1e60d66681fd52c30e5ee3
Malin Freeborn <malinfreeborn@posteo.net>
2025-06-12 18:35:10 +0200

    ensure newline after cron header

--- extra/cron.mk
+++ extra/cron.mk
@@ -4,8 +4,9 @@ default += cron.txt
 ignored += cron.txt
 
 cron.txt: $(wildcard extra/cron/*)
-	cat $^ > $@
+	cat $^ >> $@
 	grep -q '^PATH'   $@ || sed -i "1 i PATH=$(PATH)" $@
 	grep -q '^HOME'   $@ || sed -i "1 i HOME=$(HOME)" $@
 	grep -q '^MAILTO' $@ || sed -i "1 i MAILTO=$(USER)" $@
+	sed -i "2 i\ " $@
 	crontab cron.txt
