0ac28ad41052367b109f0d0d969ee83529b2bca1
Malin Freeborn <malinfreeborn@posteo.net>
2025-10-08 00:53:25 +0200

    ensure .config exists

--- extra/mime.mk
+++ extra/mime.mk
@@ -14,7 +14,7 @@ $(HOME)/.local/share/applications/mimeapps.list: $(HOME)/.config/mimeapps.list
 	@mkdir -p $(@D)
 	ln -sf $< $@
 
-$(HOME)/.config/mimeapps.list: $(local_mimetype_files)
+$(HOME)/.config/mimeapps.list: $(local_mimetype_files) | $(HOME)/.config/
 	xdg-mime default vim.desktop text/plain
 	xdg-mime default vim.desktop text/x-tex
 	xdg-mime default vim.desktop text/x-makefile
