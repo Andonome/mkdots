7b0546f53cd28e7d8256dd1a046ea0e071577a8d
Malin Freeborn <malinfreeborn@posteo.net>
2025-11-12 00:10:45 +0100

    set DISPLAY in crontab

--- extra/cron.mk
+++ extra/cron.mk
@@ -5,8 +5,11 @@ ignored += cron.txt
 
 cron.txt: $(wildcard extra/cron/*)
 	cat $^ > $@
+	sed -i "1 i\ " $@
+ifneq (${DISPLAY},)
+	grep -q '^DISPLAY' $@ || sed -i "1 i DISPLAY=$(DISPLAY)" $@
+endif
 	grep -q '^PATH'   $@ || sed -i "1 i PATH=$(PATH)" $@
 	grep -q '^HOME'   $@ || sed -i "1 i HOME=$(HOME)" $@
 	grep -q '^MAILTO' $@ || sed -i "1 i MAILTO=$(USER)" $@
-	sed -i "2 i\ " $@
 	crontab cron.txt
