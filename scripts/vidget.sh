59f810a2c2efba6ae38a220951ef5580c4213832
Malin Freeborn <malinfreeborn@posteo.net>
2026-05-10 22:53:37 +0200

    update vidscript

--- scripts/vidget.sh
+++ scripts/vidget.sh
@@ -3,29 +3,32 @@
 command -v youtube-dl 2&1>/dev/null && dl=youtube-dl
 command -v yt-dlp >/dev/null && dl='yt-dlp' && options='-ic --embed-metadata'
 
-
-mkdir -p ~/vids
+vidlist=~/.cache/vidlist.txt
 
 download_listed_video_then_remove_from_list(){
     count=0
-    [ -f .list ] && cat .list | while read link
+    [ -f "$vidlist" ] && cat "$vidlist" | while read link
     do
         count=$(( count + 1 ))
         if [ "$count" -lt 3 ]; then
             $dl $options "$link" && \
             line="$(echo $link | sed 's#/#\\/#g')" && \
-            sed -i "/$line/d" .list 
+            sed -i "/$line/d" "$vidlist" 
         fi
     done
 }
 
 warn_the_user_if_more_than_seven_videos_are_in_the_list(){
-    vidCount="$(wc -l ~/vids/.list  | cut -d' ' -f1)" 
+    vidCount="$(wc -l "$vidlist"  | cut -d' ' -f1)" 
     if [ "$vidCount" -gt 5 ]; then
         export DISPLAY=:0
         notify-send "Vidlist at $vidCount"
     fi
 }
+
+##########
+
+mkdir -p ~/vids
 
 warn_the_user_if_more_than_seven_videos_are_in_the_list
 
