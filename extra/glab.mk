0eb2ef3b7a0b64fb182b821d90a02d5f2a9f2a28
Malin Freeborn <malinfreeborn@posteo.net>
2026-08-03 22:27:29 +0200

    Glab: rebuild on theme change

--- extra/glab.mk
+++ extra/glab.mk
@@ -1,18 +1,21 @@
 token_name = gitlab-token
 
-$(HOME)/.config/glab-cli/config.yml: $(HOME)/.password-store/$(token_name).gpg
-	pass $(token_name) | glab auth login --stdin
+default += $(HOME)/.config/glab-cli/config.yml
+default += $(HOME)/.config/glab-cli/aliases.yml
+
+$(HOME)/.config/glab-cli/config.yml: $(HOME)/.password-store/$(token_name).gpg $(MAKEFILE_LIST)
+	test -f $@ || pass $(token_name) | glab auth login --stdin
+	glab config --global set glamour_style dark
 	glab config --global set editor ${VISUAL}
 	glab config --global set check_update false
 	glab config --global set browser qutebrowser
 	glab config --global set display_hyperlinks false
 
-$(HOME)/.config/glab-cli/aliases.yml:
+$(HOME)/.config/glab-cli/aliases.yml: $(HOME)/.config/glab-cli/config.yml
 	glab alias set c 'issue list'
 	glab alias set s 'issue view $1 --comments'
 	glab alias set r 'issue note $1 --message'
 
-secrets += $(HOME)/.config/glab-cli/config.yml
-secrets += $(HOME)/.config/glab-cli/aliases.yml
-
+creds += $(HOME)/.config/glab-cli/config.yml
+default += $(HOME)/.config/glab-cli/aliases.yml
 
