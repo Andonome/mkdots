cb864e86c1c4feeab303832d08494f7522b8bab1
Malin Freeborn <malinfreeborn@posteo.net>
2026-08-17 20:30:16 +0200

    Respace crontab

--- extra/cron.mk
+++ extra/cron.mk
@@ -1,16 +1,15 @@
-### Uncomment to use the makefile.
 default += cron.txt
 
 ignored += cron.txt
 
 cron.txt: $(wildcard extra/cron/*)
-	cat $^ > $@
+	sed 'i\ ' $^ > $@
 	sed -i "1 i\ " $@
 ifneq (${DISPLAY},)
-	grep -q '^DISPLAY' $@ || sed -i "1 i DISPLAY=$(DISPLAY)" $@
+	sed -i "1 i DISPLAY=$(DISPLAY)" $@
 endif
-	grep -q '^PATH'   $@ || sed -i "1 i PATH=$(PATH)" $@
-	grep -q '^HOME'   $@ || sed -i "1 i HOME=$(HOME)" $@
-	grep -q '^MAIL'   $@ || sed -i "1 i MAIL=$(MAIL)" $@
-	grep -q '^MAILTO' $@ || sed -i "1 i MAILTO=$(USER)" $@
+	sed -i "1 i PATH=$(PATH)" $@
+	sed -i "1 i HOME=$(HOME)" $@
+	sed -i "1 i MAIL=$(MAIL)" $@
+	sed -i "1 i MAILTO=$(USER)" $@
 	crontab cron.txt
