fb22331d5f76f18e0c3a0e3ec0ae053247ab4113
Malin Freeborn <malinfreeborn@posteo.net>
2026-08-17 17:46:45 +0200

    Switch email sync times

--- extra/email.mk
+++ extra/email.mk
@@ -2,3 +2,9 @@
 secrets += $(HOME)/.config/aerc/accounts.conf
 secrets += $(HOME)/.config/msmtp/config
 secrets += $(HOME)/.mbsyncrc
+default += extra/cron/email
+
+extra/cron/email:
+	echo '0 9-18 * * * mbsync -a' > $@
+	echo '10 9-18 * * * msmtpq --q-mgmt -r' >> $@
+
