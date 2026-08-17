e0e15ab336dc2991007e834a2a8ba6ff8a2a0783
Malin Freeborn <malinfreeborn@posteo.net>
2026-08-17 20:31:42 +0200

    Ignore email

--- extra/email.mk
+++ extra/email.mk
@@ -3,6 +3,7 @@ secrets += $(HOME)/.config/aerc/accounts.conf
 secrets += $(HOME)/.config/msmtp/config
 secrets += $(HOME)/.mbsyncrc
 default += extra/cron/email
+ignored += extra/cron/email
 
 extra/cron/email:
 	echo '0 9-18 * * * mbsync -a' > $@
