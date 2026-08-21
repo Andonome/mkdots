f88e3e0fa494661278e3f772d05d080d075abfbe
Malin Freeborn <malinfreeborn@posteo.net>
2026-08-21 03:07:52 +0200

    Email: more pulls

--- extra/email.mk
+++ extra/email.mk
@@ -1,11 +1,11 @@
+extra/cron.mk: extra/cron/email
 
 secrets += $(HOME)/.config/aerc/accounts.conf
 secrets += $(HOME)/.config/msmtp/config
 secrets += $(HOME)/.mbsyncrc
-default += extra/cron/email
 ignored += extra/cron/email
 
 extra/cron/email:
-	echo '0 9-18 * * * mbsync -a' > $@
+	echo '0 9-18,19-3/3 * * * mbsync -a' > $@
 	echo '10 9-18 * * * msmtpq --q-mgmt -r' >> $@
 
